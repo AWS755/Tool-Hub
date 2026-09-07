789d5edd04a82510cacf51fa57a597efab320019
Abdulrehman <abdulrahmanwaeel@gmail.com>
2026-09-07 21:36:55 +0300

    Update script.js

--- ضغط pdf/script.js
+++ ضغط pdf/script.js
@@ -10,7 +10,7 @@
    Configuration
 ========================================================= */
 
-const API_BASE_URL = "http://127.0.0.1:5000";
+const API_BASE_URL = "https://cairo1455.pythonanywhere.com";
 
 const COMPRESS_API_URL =
     `${API_BASE_URL}/api/compress-pdf`;
